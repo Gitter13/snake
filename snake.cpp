1ec93d645401d8d465c3ea2a36f577bc29255bd6
Gitter13 <anna.voracova.2009@gmail.com>
2025-12-18 12:05:38 +0100

    multiplayer funguje, funguje i pri vraceni zpatky ze neni smrt

--- snake.cpp
+++ snake.cpp
@@ -4,13 +4,15 @@
 #include <string>
 using namespace std;
 
-const float obnova_frame = 0.1;
+const float obnova_frame = 0.5;
 const float min_cas_jidla = 1.0f;
 
 const int sirka = 600;
 const int vyska = 600;
 bool death = false;
 int snezeno = 0;
+bool hrajeme_dva = false;
+int kdo_vyhral = 0;
 
 const int velikost_bloku = 60;
 
@@ -18,17 +20,20 @@ const int velikost_bloku = 60;
 class Parts {
 public:
     Vector2 position;
+    Color barva;
     char smer; // 'U' - up, 'D' - down, 'L' - left, 'R' - right
-    Parts(Vector2 posi, char ismer){
+    Parts(Vector2 posi, char ismer, Color ibarva){
         position = posi;
         smer = ismer;
+        barva = ibarva;
     }
 
     void draw() {
-        DrawRectangle(position.x, position.y, velikost_bloku, velikost_bloku, GREEN);
+        DrawRectangle(position.x, position.y, velikost_bloku, velikost_bloku, barva);
     }
 };
 vector<Parts> snake;
+vector<Parts> snake_dva;
 
 class Food {
 public:
@@ -49,20 +54,87 @@ public:
             snezeno++;
             return true;
         }
+        if (hrajeme_dva &&(position.x == snake_dva[0].position.x && position.y == snake_dva[0].position.y)) {
+            // Add new part to snake
+            Parts newPart = snake_dva.back();
+            snake_dva.push_back(newPart);
+            snezeno++;
+            return true;
+        }
         return false;
     }
+    
 };
 
 vector<Food> foods;
 float casovac_jidla = 2.0f;
+
+Vector2 check_collision(vector<Parts> &snake){
+    //chekovani kolizi se steny sebou a presun casti hada
+    // Move snake parts
+    bool mrtev = false; //local death flag jestli umre tento had
+    Parts &head = snake[0];
+    float predpoved_y = head.position.y;
+    float predpoved_x = head.position.x;
+        switch (head.smer) {
+            case 'U': predpoved_y -= velikost_bloku; break;
+            case 'D': predpoved_y += velikost_bloku; break;
+            case 'L': predpoved_x -= velikost_bloku; break;
+            case 'R': predpoved_x += velikost_bloku; break;
+        }
+    if (snake.size() > 1){
+        //co když jde nazpátek?
+        if (predpoved_x == snake[1].position.x && predpoved_y == snake[1].position.y) {
+            predpoved_y = head.position.y;
+            predpoved_x = head.position.x;
+            switch (snake[0].smer) {
+                case 'U': predpoved_y += velikost_bloku; break; //musí jít opačně
+                case 'D': predpoved_y -= velikost_bloku; break;
+                case 'L': predpoved_x += velikost_bloku; break;
+                case 'R': predpoved_x -= velikost_bloku; break;
+            }
+        }
+    }
+    //check collisions with itself
+    for (int i = 2; i < snake.size()-1; ++i) {
+        if (predpoved_x == snake[i].position.x && predpoved_y == snake[i].position.y) {
+            death = true;
+            mrtev = true;
+            break;
+        }
+    }
+    // Check collisions with walls     
+    if (predpoved_x < 0) death = true, mrtev = true;
+    if (predpoved_x > sirka - velikost_bloku) death = true, mrtev = true;
+    if (predpoved_y < 0) death = true, mrtev = true;
+    if (predpoved_y > vyska - velikost_bloku) death = true, mrtev = true;
+
+    if (mrtev) {
+        if (hrajeme_dva) {
+            if (&snake == &snake_dva) {
+                if (kdo_vyhral == 2) {
+                    kdo_vyhral = 12;
+                } else{
+                    kdo_vyhral = 1;
+                }
+            } else {
+                kdo_vyhral = 2;
+            }
+        }
+    }
+    return {predpoved_x, predpoved_y};
+        
+}
 
 int main() {
     InitWindow(sirka, vyska, "Snake Game");
     SetTargetFPS(60);
 
     // Initialize snake with one part
-    snake.push_back(Parts({0, 0}, 'R'));
-
+    snake.push_back(Parts({0, 0}, 'R', GREEN));
+    if (hrajeme_dva) {
+        snake_dva.push_back(Parts({sirka - velikost_bloku, vyska - velikost_bloku}, 'L', YELLOW));
+    }
     float cas_od_posledni_obnovy = 0.0f;
 
     while (!WindowShouldClose()) {
@@ -81,7 +153,20 @@ int main() {
             }
             if (IsKeyPressed(KEY_DOWN)) {
                 snake[0].smer = 'D';
-                cout << snake[0].smer << endl;
+            }
+            if (hrajeme_dva) {
+                if (IsKeyPressed(KEY_A)) {
+                    snake_dva[0].smer = 'L';
+                }
+                if (IsKeyPressed(KEY_D)) {
+                    snake_dva[0].smer = 'R';
+                }
+                if (IsKeyPressed(KEY_W)) {
+                    snake_dva[0].smer = 'U';
+                }
+                if (IsKeyPressed(KEY_S)) {
+                    snake_dva[0].smer = 'D';
+                }
             }
             if (casovac_jidla <= 0.0f) {
                 // Add new food
@@ -97,49 +182,54 @@ int main() {
                         foods.erase(foods.begin()+i);
                     }
                 }
-                
-                // Move snake parts
-                Parts &head = snake[0];
-                float predpoved_y = head.position.y;
-                float predpoved_x = head.position.x;
-                switch (head.smer) {
-                    case 'U': predpoved_y -= velikost_bloku; break;
-                    case 'D': predpoved_y += velikost_bloku; break;
-                    case 'L': predpoved_x -= velikost_bloku; break;
-                    case 'R': predpoved_x += velikost_bloku; break;
-                }
-
-                //check collisions with itself
-                for (size_t i = 1; i < snake.size(); ++i) {
-                    if (predpoved_x == snake[i].position.x && predpoved_y == snake[i].position.y) {
-                        if (i==1){
-                            predpoved_y = head.position.y;
-                            predpoved_x = head.position.x;
-                            switch (snake[0].smer) {
-                                case 'U': predpoved_y += velikost_bloku; break; //musí jít opačně
-                                case 'D': predpoved_y -= velikost_bloku; break;
-                                case 'L': predpoved_x += velikost_bloku; break;
-                                case 'R': predpoved_x -= velikost_bloku; break;
+                Vector2 zkouska_snake = check_collision(snake);
+                Vector2 zkouska_snake_dva;
+                //check collision for two snakes
+                if (hrajeme_dva) {
+                    zkouska_snake_dva = check_collision(snake_dva);
+                    for (int j=0; j<snake.size()-1; j++) {
+                        if(snake[j].position.x == zkouska_snake_dva.x && snake[j].position.y == zkouska_snake_dva.y){
+                            death = true;
+                            if (kdo_vyhral == 0) {
+                                kdo_vyhral = 1;
+                            } else if (kdo_vyhral == 2){
+                                kdo_vyhral = 12;
                             }
                             break;
-                        } else{
+                        }
+                    }
+                    if (zkouska_snake.x == zkouska_snake_dva.x && zkouska_snake.y == zkouska_snake_dva.y){
+                        death = true;
+                        kdo_vyhral = 12;
+                    }
+                    for (int j=0; j<snake_dva.size()-1; j++) {
+                        if(snake_dva[j].position.x == zkouska_snake.x && snake_dva[j].position.y == zkouska_snake.y){
                             death = true;
+                            if (kdo_vyhral == 0) {
+                                kdo_vyhral = 2;
+                            } else if (kdo_vyhral == 1){
+                                kdo_vyhral = 12;
+                            }
                             break;
                         }
                     }
                 }
-                // Check collisions with walls     
-                if (predpoved_x < 0) death = true;
-                if (predpoved_x > sirka - velikost_bloku) death = true;
-                if (predpoved_y < 0) death = true;
-                if (predpoved_y > vyska - velikost_bloku) death = true;
+                // Move snake parts
                 if (!death) {
                     for (size_t i = snake.size() - 1; i > 0; --i) {
                         snake[i].position = snake[i - 1].position;
                         snake[i].smer = snake[i - 1].smer;
                     }
-                    head.position.x = predpoved_x;
-                    head.position.y = predpoved_y;
+                    snake[0].position.x = zkouska_snake.x;
+                    snake[0].position.y = zkouska_snake.y;
+                    if (hrajeme_dva) {
+                        for (size_t i = snake_dva.size() - 1; i > 0; --i) {
+                            snake_dva[i].position = snake_dva[i - 1].position;
+                            snake_dva[i].smer = snake_dva[i - 1].smer;
+                        }
+                        snake_dva[0].position.x = zkouska_snake_dva.x;
+                        snake_dva[0].position.y = zkouska_snake_dva.y;
+                    }
                 }
                 cas_od_posledni_obnovy = 0.0f;
             }
@@ -149,6 +239,36 @@ int main() {
         DrawText(("Snezene jidlo: " + to_string(snezeno)).c_str(), 10, 10, 20, WHITE);
         if (death) {
             DrawText("Game Over!", sirka / 2 - 80, vyska / 2 - 20, 40, RED);
+            DrawText("Stiskni ENTER pro restart hry.", sirka / 2 - 150, vyska / 2 + 30, 20, LIGHTGRAY);
+            if (kdo_vyhral != 0) {
+                if (kdo_vyhral == 12) {
+                    DrawText("Remiza!", sirka / 2 - 60, vyska / 2 + 30, 30, ORANGE);
+                } else {
+                    if (kdo_vyhral == 1) DrawText("Vyhral zeleny hrac!", sirka / 2 - 100, vyska / 2 + 30, 30, GREEN);
+                    else if (kdo_vyhral == 2) DrawText("Vyhral zluty hrac!", sirka / 2 - 100, vyska / 2 + 30, 30, YELLOW);
+                }
+            }
+            if (hrajeme_dva) {
+                DrawText("HRA PRO DVA.", sirka / 2 - 220, vyska / 2 + 70, 20, LIGHTGRAY);
+            } else {
+                DrawText("Stiskni MEZERNIK pro hru pro dva.", sirka / 2 - 100, vyska / 2 + 70, 20, LIGHTGRAY);
+            }
+            if (IsKeyPressed(KEY_SPACE)){
+                hrajeme_dva = !hrajeme_dva;
+            }
+            if (IsKeyPressed(KEY_ENTER)) {
+                // Reset game
+                snake.clear();
+                snake_dva.clear();
+                snake.push_back(Parts({0, 0}, 'R', GREEN));
+                if (hrajeme_dva) {
+                    snake_dva.push_back(Parts({sirka - velikost_bloku, vyska - velikost_bloku}, 'L', YELLOW));
+                }
+                foods.clear();
+                snezeno = 0;
+                death = false;
+                kdo_vyhral = 0;
+            }
         }
 
         // Draw food
@@ -158,6 +278,11 @@ int main() {
         // Draw snake
         for (auto &part : snake) {
             part.draw();
+        }
+        if (hrajeme_dva) {
+            for (auto &part : snake_dva) {
+                part.draw();
+            }
         }
 
         EndDrawing();
